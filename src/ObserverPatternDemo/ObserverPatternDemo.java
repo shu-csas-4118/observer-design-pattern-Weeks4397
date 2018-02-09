4acbffe191498db1b11ff37802861d40ce8bfa69
Weeks4397 <35846723+Weeks4397@users.noreply.github.com>
2018-02-09 12:50:40 -0500

    Update ObserverPatternDemo.java

--- src/ObserverPatternDemo/ObserverPatternDemo.java
+++ src/ObserverPatternDemo/ObserverPatternDemo.java
@@ -3,22 +3,23 @@ package ObserverPatternDemo;
 public class ObserverPatternDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Student student = new Student("Garett", "Chang", 123456, "changgar@shu.edu");
 		
 		IdNumberObserver idObserver =  new IdNumberObserver();
 		FirstNameObserver firstNameObserver = new FirstNameObserver();
+		LastNameObserver lastNameObserver = new LastNameObserver();
 		EmailObserver emailObserver = new EmailObserver();
 		CourseObserver courseObserver = new CourseObserver();
 		
 		student.attachObserver(idObserver);
 		student.attachObserver(firstNameObserver);
+		student.attachObserver(lastNameObserver);
 		student.attachObserver(emailObserver);
 		student.attachObserver(courseObserver);
 		
-		student.addcources("2018_SPRING_CSAS4118AA Software Engineering II");
-		student.addcources("2018_SPRING_CSAS4118AA Software Engineering II");
-		student.addcources("2018_SPRING_CSAS4118AA Software Engineering II");
+		student.addCources("2018_SPRING_CSAS4118AA Software Engineering II");
+		student.addCources("2018_SPRING_CSAS4113BA Formal Lang and Automata");
+		student.addCources("2018_SPRING_DAVA3010WB Data Mining");
 		student.setFirstName("Alex");
 		student.setLastName("Weeks");
 		student.setIdNumber(543216);
