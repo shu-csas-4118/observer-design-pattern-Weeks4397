169266d11235755a159cf11ae26e992c075c04a9
Weeks4397 <35846723+Weeks4397@users.noreply.github.com>
2018-02-09 12:49:54 -0500

    Update CourseObserver.java

--- src/ObserverPatternDemo/CourseObserver.java
+++ src/ObserverPatternDemo/CourseObserver.java
@@ -8,6 +8,7 @@ public class CourseObserver implements IObserver {
 	
 	public void update(Student student, String NeedToNotify) {
 		if(NeedToNotify.equalsIgnoreCase("course")) {
+			System.out.println("Student's current courses are:");
 			for(int i = 0; i < student.getcources().size(); i++) {
 				System.out.println("Course "+ (i + 1) +": " + student.getcources().get(i));
 			}	
